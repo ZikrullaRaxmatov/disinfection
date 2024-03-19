5dd9ffd33971abe1965df3f29aa6b2f1144a922f
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-03-19 22:40:44 +0900

    #

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -12,7 +12,7 @@ function Header() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#!">Main</a>
                         </li>
