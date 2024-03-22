37f03ff44e5fcb73bcf20cec5c78609aebafec58
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-03-22 14:56:14 +0900

    #

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -13,8 +13,8 @@ function Main() {
                 <div className='col-12 col-md-8  d-flex justify-content-center align-items-center'>
                     <img data-aos="zoom-in" data-aos-duration="3000" src={imgMain} alt='mainImage' className='w-100 border border-success rounded shadow'/>
                 </div>
-                
-                <div className='border border-danger z-1 position-absolute col-md-4 text-center z-0 top-2 end-0 p-4' data-aos="fade-left" data-aos-duration="5000" >
+                {/* position-absolute */}
+                <div className='border border-danger z-1  col-md-4 text-center z-0 top-2 end-0 p-4' data-aos="fade-left" data-aos-duration="5000" >
                     <h1 className='mt-4 mt-lg-5'>{t("main.title")}</h1>
                     <p className='mt-4' >{t("main.body")}</p>
                     <a href="#contact" className=' btn mt-4 mt-lg-5' style={{ backgroundColor: "#5cad00", color: "white" }} >{t("main.btn")}</a>
